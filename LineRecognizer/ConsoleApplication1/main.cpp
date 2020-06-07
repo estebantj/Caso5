f887725f20d0831099115ef41ae1ad5d307bf16e
Jose <mikomeka1@gmail.com>
2020-06-07 03:26:32 -0600

    Deleting unnecesary files

--- LineRecognizer/ConsoleApplication1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "LineRecognizer.h"
-
-int main()
-{
-	std::vector<std::array<int, 4>> lineas;
-	LineRecognizer * algoritmito = new LineRecognizer();
-	algoritmito->ordenarSectores(lineas);
-}
