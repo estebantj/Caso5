9f034052bb82965dd84fe49232f5996058f4b5dd
Esteban Torres <esteban13torres@gmail.com>
2020-06-09 16:45:49 -0600

    Se comentaron las funciones

--- Variables.h
+++ Variables.h
@@ -5,10 +5,11 @@
 
 
 const int anchuraPantalla = 1080,
-	alturaPantalla = 920,
-	cantidadSectoresX = 20,
-	cantidadSectoresY = 20,
-	;
+alturaPantalla = 920,
+cantidadSectoresX = 20,
+cantidadSectoresY = 20;
 
+const int cantidadLineasTotal = 50,
+cantidadLineasBuenas = 20;
 
 #endif // Variable_H
